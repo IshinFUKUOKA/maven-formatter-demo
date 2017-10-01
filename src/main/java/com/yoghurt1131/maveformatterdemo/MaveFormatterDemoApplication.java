cf9f14af470bcdcbd6ce9b5e0e3b57ed28f39403
IshinFUKUOKA <yoghurt1131@gmail.com>
2017-10-01 18:28:37 +0900

    initial commit

---
+++ src/main/java/com/yoghurt1131/maveformatterdemo/MaveFormatterDemoApplication.java
@@ -0,0 +1,12 @@
+package com.yoghurt1131.maveformatterdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaveFormatterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaveFormatterDemoApplication.class, args);
+	}
+}
